9f8e2c5f9512f37ece620a74e5b609d33f9e148f
Erick Benezar <160497694+erickbenezar218@users.noreply.github.com>
2024-11-16 15:58:10 -0400

    Create macos-setup.sh

---
+++ macos-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Instalar Cyberduck (alternativa ao WinSCP)
+echo "Instalando o Cyberduck (alternativa ao WinSCP)..."
+brew install --cask cyberduck
+
+# Instalar Anydesk 
+echo "Instalando o Anydesk..."
+brew install --cask anydesk
+
+# Instalar Discord (alternativa ao Slack)
+echo "Instalando o Discord..."
+brew install --cask discord
+
+# Instalar Spotify (alternativa de música)
+echo "Instalando o Spotify..."
+brew install --cask spotify
+
+# Instalar Git (para desenvolvimento)
+echo "Instalando o Git..."
+brew install git
+
+# Instalar Docker (para DevOps)
+echo "Instalando o Docker..."
+brew install --cask docker
+
+# Instalar Terraform (para infraestrutura como código - DevOps)
+echo "Instalando o Terraform..."
+brew install terraform
+
+# Instalar Nmap (ferramenta de rede, útil para análise de redes)
+echo "Instalando o Nmap..."
+brew install nmap
+
+# Instalar Wireshark (ferramenta para captura e análise de pacotes de rede)
+echo "Instalando o Wireshark..."
+brew install --cask wireshark
+
+# Instalar Trello (gestão de tarefas e projetos)
+echo "Instalando o Trello..."
+brew install --cask trello
+
+# Finalizando
+echo "Instalações concluídas com sucesso!"
